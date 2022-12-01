27357d3beead8fed49a17633f22f774a56397ab1
siyenga1 <siyenga1@binghamton.edu>
2022-12-01 10:56:31 -0500

    Commit : comments

--- kmp/kmp.py
+++ kmp/kmp.py
@@ -1,13 +1,17 @@
 class KMP:
-    # KMP class that has
-    # myInput for taking user input
-    # myLCSTable for storing the prefix values
-    # Kmp Search method use the table and search for pattern
+    """
+     KMP class that has
+     myInput for taking user input
+     myLCSTable for storing the prefix values
+     Kmp Search method use the table and search for pattern
+    """
 
     def myInput(self):
         count1 = 0
         count2 = 0
-        # Takes user inputs and checks for two strings
+        """
+        Takes user inputs and checks for two strings
+        """
         print("*********************WELCOME TO KNUTH MORRIS PATTERN SEARCH ALGORITHM*******************")
         # The input string and the pattern to be matched with
         string = input("Please enter the first input string ")
@@ -15,9 +19,13 @@ class KMP:
         if len(string) == 0 or len(pattern) == 0:
             print("Empty String or Empty Pattern")
             return
-        # calls the default constructor
+        """
+        calls the default constructor
+        """
         positionOfPattern = KMP()
+        """
         # listOfindex stores the positions/locations
+        """
         listOfindex1 = positionOfPattern.kmpSearch(pattern, string)
         if not listOfindex1:
             print("Pattern not found")
@@ -26,7 +34,10 @@ class KMP:
                 print("The strings are the same and found at location", x)
                 count1 += 1
             print("The number of times the pattern is found = ", count1)
-
+        """
+        Two strings to check different kinds of inputs...
+        For ex: to check case sensitive and insensitive.
+        """
         string2 = input("Please enter the second input string ")
         pattern2 = input("Please enter the pattern to be matched ")
         if len(string2) == 0 or len(pattern2) == 0:
@@ -43,9 +54,10 @@ class KMP:
             print("The number of times the pattern is found = ", count2)
 
     def myLCSTable(self, pattern, sizeOfPattern):
-        # LCS table to store the pattern that's already been discovered
-
-        # initialize table to 0 values
+        """
+        LCS table to store the pattern that's already been discovered
+        initialize this table to 0 values
+        """
         lcsTable = [0] * sizeOfPattern
         i = 1
         j = 0
@@ -62,32 +74,49 @@ class KMP:
         return lcsTable
 
     def kmpSearch(self, pattern, string):
-        # to check for length of the text and pattern
+        """
+        To check for length of the text and pattern
+        respective variables: sizeOfPattern and string
+        """
         sizeOfPattern = len(pattern)
         sizeOfString = len(string)
 
-        # LCS Table
+        """
+        Invokes the LCS table to store pattern along
+        with length of the pattern computed
+        """
         lcsTable = self.myLCSTable(pattern, sizeOfPattern)
-
-        # Pointers for string text and pattern
+        """
+        Pointer for string text as i
+        and pointer for pattern as j
+        """
         patternPos = []
         i = 0;
         j = 0;
 
         while i != sizeOfString:
-            # Makes sure the strings are case-insensitive
+            """
+            casefold() makes sure the strings are case-insensitive
+            """
             if string[i].casefold() == pattern[j].casefold():
                 i = i + 1;
                 j = j + 1;
             else:
-                # Pattern not found, decrement j in the table
+                """
+                Pattern not found, 
+                decrement j in the table
+                """
                 j = lcsTable[j - 1]
-                # if reached the end of the pattern j=sizeofPattern
-                # print index i-j
+                """
+                if reached the end of the pattern j=sizeofPattern
+                print index i-j
+                """
             if j == sizeOfPattern:
                 patternPos.append(i - j)
                 j = lcsTable[j - 1]
-                # Reached the end of string but j is zero
+                """
+                Reached the end of string but j is zero
+                """
             elif j == 0:
                 i += 1;
 
